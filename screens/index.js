17bbbd76b6b8ceaf96c1d784b87e4ad892125c01
Akbar Sirajuddin Hanif <akbarsirajuddinh@.com>
2018-11-14 18:52:40 +0700

    first commit

---
+++ screens/index.js
@@ -0,0 +1,19 @@
+import { Navigation } from 'react-native-navigation';
+
+import Home from './Home';
+import Heroes from './Heroes';
+import Settings from './Setting';
+
+
+// //pushed screen
+import HeroAdd from './HeroAdd';
+import HeroView from './HeroView';
+
+// register all screens of the app (including internal ones)
+export function registerScreens() {
+  Navigation.registerComponent('tab.Home', () => Home);
+  Navigation.registerComponent('tab.Heroes', () => Heroes);
+  Navigation.registerComponent('tab.Settings', () => Settings);
+  Navigation.registerComponent('push.HeroAdd', () => HeroAdd);
+  Navigation.registerComponent('push.HeroView', () => HeroView);
+}
